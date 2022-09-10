56ab309c9ebe5eb24c03c41e692e2a879629bcc7
PremaJyo <109197934+PremaJyo@users.noreply.github.com>
2022-09-10 16:25:34 +0530

    Add files via upload

---
+++ work.js
@@ -0,0 +1,11 @@
+var nItems=document.getElementById("nav-items");
+console.log(nItems);
+function navItemsDisplay(){
+    nItems.style.display="block";
+    console.log("Good");
+
+}
+function displayE(){
+    document.getElementById("navBar").classList.toggle('displayC');
+}
+
